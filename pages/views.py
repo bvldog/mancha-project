6830fff23cc652e95c3e17f60674d2a8d8e4078d
bvldog <bvldog@gmail.com>
2021-07-02 13:41:54 +0900

    navbar set

--- pages/views.py
+++ pages/views.py
@@ -1,5 +1,17 @@
 from django.shortcuts import render
 
 
-def pages(request):
+def home(request):
     return render(request, "pages/home.html")
+
+
+def about(request):
+    return render(request, 'pages/about.html')
+
+
+def service(request):
+    return render(request, 'pages/service.html')
+
+
+def contact(request):
+    return render(request, 'pages/contact.html')
